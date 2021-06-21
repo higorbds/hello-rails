d92fe6ef427371700e765714c7b64fbba60a6d87
Higor Bueno da Silva <h.higorbsilva.dev@gmail.com>
2021-06-20 23:47:09 -0300

    Initial Commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", only: :destory
+  
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comments_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
+  end
+  
+
+  private
+
+  def comments_params
+    params.require(:comment).permit(:commenter, :body, :status)
+  end
+  
+end
