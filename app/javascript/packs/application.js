3b14c08f2c1b6f470e42d61904f21f81b6a963b1
Higor Bueno da Silva <h.higorbsilva.dev@gmail.com>
2021-06-22 11:18:13 -0300

    Fixing importation of bulma library

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "bulma"
+import "bulma/css/bulma.min.css"
 
 Rails.start()
 Turbolinks.start()
