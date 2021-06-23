ccb784463c29feae94d485cc28c63ea2d391edcc
Higor Bueno da Silva <h.higorbsilva.dev@gmail.com>
2021-06-23 20:55:46 -0300

    fixing a importation of bulma.io

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "bulma/css/bulma.min.css"
 
 Rails.start()
 Turbolinks.start()
